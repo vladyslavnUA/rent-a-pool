125e88405ea7f3400fe9f3f74a5162b5166d8434
vladyslavnUA <vladyslavn@yahoo.com>
2019-12-26 15:00:24 -0500

    updated app.py

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/pools')
+client = MongoClient(host=host)
+db = client.get_default_database()
+pools = db.pools
+
+
+app = Flask(__name__)
+
+# @app.route("/login")
+# def login:
+# 	auth = request.authorization
+# 	return " "
+
+@app.route("/")
+def pools_index():
+	#this will show the type of pants we have
+	return render_template("pools_index.html", pools=pools.find())
+
+@app.route("/pools", methods=["POST"])
+def pools_submit():
+	pool = {
+		"pool_name": request.form.get("pool_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+        	"brand": request.form.get("brand")
+	}
+	pool_id = pools.insert_one(pool).inserted_id
+	print(pool_id)
+	return redirect(url_for("pools_show", pool_id = pool_id))
+
+@app.route("/pools/<pool_id>")
+def pools_show(pool_id):
+	pool = pools.find_one({'_id' : ObjectId(pool_id)})
+	return render_template("pools_show.html", pool = pool)
+
+@app.route("/pools/new")
+def pools_new():
+	return render_template("pools_new.html", pool={}, title ="New Item")
+
+@app.route("/pools/<pool_id>/edit")
+def pools_edit(pool_id):
+	pool = pools.find_one({"_id" : ObjectId(pool_id)})
+	return render_template("pools_edit.html", pool = pool, title = "Edit Item")
+
+@app.route("/pools/<pool_id>", methods = ['POST'])
+def pools_update(pool_id):
+	updated_pool = {
+		"pool_name": request.form.get("pool_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+        	"brand": request.form.get("brand")
+	}
+
+	pools.update_one( {"_id" : ObjectId(pool_id)}, {"$set" : updated_pool})
+	return redirect(url_for("pools_show", pool_id = pool_id))
+
+@app.route("/pools/<pool_id>/delete", methods=["POST"])
+def pools_delete(pool_id):
+	pools.delete_one({"_id" : ObjectId(pool_id)})
+	return redirect(url_for("pools_index"))
+
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
